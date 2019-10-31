04d43a970a3148a246234a267ddc610f4cec1335
Bharat Agarwal <43988371+bharatagsrwal@users.noreply.github.com>
2019-10-31 21:13:01 +0530

    updated some animations and text to generalized

--- message/js/index.js
+++ message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "THIS IS MESSAGE SECTION OF THIS WEBSITE.DEMO MESSAGE-  A 19 years of madness and I know this isn't going to stop as age is just a Number. A birthday like today must be memorable and unforgettable. Never lose your stunning smile!. You always shine like the sun and spread joy and love to everyone around you. You are one of the best souls we've ever met. What else can we say, Crazy Girl who Cares a lot ❤, Thank You So Much for being a part for our life. HAPPY BIRTHDAY stay blessed keeping ROCK and ROLLING. ___ NOTE : In next page, the 'NEXT' BUTTON is in the bottom of the page, scroll to see it n proceed...................";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "THIS IS MESSAGE SECTION OF THIS WEBSITE.DEMO MESSAGE- Your custom message";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
@@ -17,6 +17,6 @@ function animateText(){
 	});
 	TweenMax.staggerTo(animatedSpan, .2,{
 		opacity : 1,
-		delay : 1
+		delay : 2
 	},0.05);
 }
